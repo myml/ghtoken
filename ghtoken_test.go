8e7ba9ffbae10273bac7c64b36ee958e5b7a6c38
myml <wurongjie1@gmail.com>
2022-05-06 11:12:39 +0800

    feat: use GITHUB_ACTOR

--- ghtoken_test.go
+++ ghtoken_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/google/go-github/github"
@@ -12,7 +13,7 @@ import (
 func TestGitHubToken(t *testing.T) {
 	tr := NewGitHubToken(http.DefaultTransport)
 	client := github.NewClient(&http.Client{Transport: tr})
-	user, _, err := client.Users.Get(context.Background(), "myml")
+	user, _, err := client.Users.Get(context.Background(), os.Getenv("GITHUB_ACTOR"))
 	if err != nil {
 		t.Fatal(err)
 	}
