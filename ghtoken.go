361a4b80e1d9948f7a67fe10f783aaa52244daff
myml <wurongjie@deepin.org>
2023-01-05 11:43:24 +0800

    feat: Support multiple token

--- ghtoken.go
+++ ghtoken.go
@@ -1,19 +1,32 @@
 package ghtoken
 
 import (
+	"math/rand"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 type GitHubToken struct {
-	tr http.RoundTripper
+	tr     http.RoundTripper
+	rand   *rand.Rand
+	tokens []string
 }
 
 func NewGitHubToken(tr http.RoundTripper) *GitHubToken {
-	return &GitHubToken{tr: tr}
+	return &GitHubToken{
+		tr:     tr,
+		tokens: strings.Split(os.Getenv("GITHUB_TOKEN"), ","),
+		rand:   rand.New(rand.NewSource(time.Now().Unix())),
+	}
 }
 
 func (gh *GitHubToken) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.Header.Add("Authorization", "Bearer "+os.Getenv("GITHUB_TOKEN"))
+	if len(gh.tokens) == 0 {
+		return gh.tr.RoundTrip(req)
+	}
+	token := gh.tokens[gh.rand.Intn(len(gh.tokens))]
+	req.Header.Add("Authorization", "Bearer "+token)
 	return gh.tr.RoundTrip(req)
 }
